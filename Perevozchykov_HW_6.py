63c4a1e210564b4aa5b53665a0ec3a2bb7847adf
Grygorii <perevozchik89@gmail.com>
2020-10-25 23:00:00 +0200

    added func to determine the number of letters in  line

--- Perevozchykov_HW_6.py
+++ Perevozchykov_HW_6.py
@@ -1,3 +1,20 @@
+def func(text):
+    letters = 0
+    for symbol in text:
+        letters += 1
+    print(line,letters,"симв.","\n")
+
+
 with open('MyParsing',encoding='UTF-8') as pars:
-    pars.readline()
+    for line in pars.readlines():
+        func(line)
+
+
+
+
+
+
+
+
+
 
