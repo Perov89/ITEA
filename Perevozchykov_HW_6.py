1a1f284329c9dd833890f76b67d00026bf87855a
Grygorii <perevozchik89@gmail.com>
2020-10-26 22:32:00 +0200

    Убрал комментарии с первого задания

--- Perevozchykov_HW_6.py
+++ Perevozchykov_HW_6.py
@@ -1,20 +1,20 @@
-# def func(text):
-#     letters = 0
-#     words = 0
-#     for symbol in text:
-#         letters += 1
-#     for word in text.split(' '):
-#         words += 1
-#     print(line, letters, "симв.", words, 'сл.', "\n")
-#
-#
-#
-# with open('MyParsing',encoding='UTF-8') as pars:
-#     stroka = 0
-#     for line in pars.readlines():
-#         func(line)
-#         stroka +=1
-#     print(stroka,'строки')
+def func(text):
+    letters = 0
+    words = 0
+    for symbol in text:
+        letters += 1
+    for word in text.split(' '):
+        words += 1
+    print(line, letters, "симв.", words, 'сл.', "\n")
+
+
+
+with open('MyParsing',encoding='UTF-8') as pars:
+    stroka = 0
+    for line in pars.readlines():
+        func(line)
+        stroka +=1
+    print(stroka,'строки')
 # ______________________________________________________________________________________________________________________
 import re
 
